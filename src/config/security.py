c3021981bddcd178c1d5576f7df2809a06eedbe9
Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
2025-04-16 16:41:27 +0200

    More config updates

--- src/config/security.py
+++ src/config/security.py
@@ -22,18 +22,21 @@ def configure_talisman(app):
         Add CSP and other headers allowing Flasgger Swagger UI to function.
         """
         if 'flasgger_static' in request.path or '/apidocs' in request.path:
+            print("Adding relaxed CSP headers for Swagger route: {request.path}")
             response.headers['Content-Security-Policy'] = (
                 "default-src 'self'; "
-                "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net; "
-                "style-src-elem 'self' https://fonts.googleapis.com https://cdn.jsdelivr.net; "
                 "script-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net; "
-                "script-src-elem 'self' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net; "
+                "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net; "
                 "font-src 'self' https://fonts.gstatic.com https://migaku-public-data.migaku.com; "
                 "img-src 'self' data:; "
                 "connect-src 'self'; "
-                "frame-ancestors 'none';"
-            )
+                "frame-ancestors 'none'; "
+                "object-src 'none'; "
+                "base-uri 'self';" 
+        )
+
         else:
+            print("Adding strict CSP headers for non-Swagger route: {request.path}")
             response.headers['Content-Security-Policy'] = "default-src 'self';"
             response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
             response.headers['X-Content-Type-Options'] = 'nosniff'
